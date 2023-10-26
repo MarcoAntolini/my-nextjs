45d744596feb517c26b770f346428c1e6634276d
Marco Antolini <marcoantolini.dev@gmail.com>
2023-10-26 12:06:47 +0200

    remove unused settings

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	async rewrites() {
-		return {
-			fallback: [
-				{
-					source: "/:path*",
-					destination: `https://marcoantolini.com/next-app-template/:path*`,
-				},
-			],
-		};
-	},
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
